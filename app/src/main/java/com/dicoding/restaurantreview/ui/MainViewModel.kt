f9ed85bc7951bb077698682660d78d656dca5125
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2024-03-15 09:39:45 +0700

    Merubah aplikasi review restaurant menjadi survive terhadap configuration changes dengan menggunakan viewModel dan LiveData

---
+++ app/src/main/java/com/dicoding/restaurantreview/ui/MainViewModel.kt
@@ -0,0 +1,89 @@
+package com.dicoding.restaurantreview.ui
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dicoding.restaurantreview.data.response.CustomerReviewsItem
+import com.dicoding.restaurantreview.data.response.PostReviewResponse
+import com.dicoding.restaurantreview.data.response.Restaurant
+import com.dicoding.restaurantreview.data.response.RestaurantResponse
+import com.dicoding.restaurantreview.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+    // variabel untuk menampung data restoran, review, dan status loading.
+    private val _restaurant = MutableLiveData<Restaurant>()
+    val restaurant: LiveData<Restaurant> = _restaurant
+
+    private val _listReview = MutableLiveData<List<CustomerReviewsItem>>()
+    val listReview: LiveData<List<CustomerReviewsItem>> = _listReview
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object {
+        // konstanta TAG dan RESTAURANT_ID
+        private const val TAG = "MainViewModel"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+
+    }
+
+    init {
+        findRestaurant()
+        postReview(review = String())
+    }
+
+    // pindahkan fungsi findRestaurant yang ada di MainActivity ke MainViewModel
+    private fun findRestaurant() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
+        client.enqueue(object : Callback<RestaurantResponse> {
+            override fun onResponse(
+                call: Call<RestaurantResponse>,
+                response: Response<RestaurantResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _restaurant.value = response.body()?.restaurant
+                    _listReview.value = response.body()?.restaurant?.customerReviews
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+
+    fun postReview(review: String) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "rashif", review)
+        client.enqueue(object : Callback<PostReviewResponse> {
+            override fun onResponse(
+                call: Call<PostReviewResponse>,
+                response: Response<PostReviewResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _listReview.value = response.body()?.customerReviews
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+
+    }
+}
